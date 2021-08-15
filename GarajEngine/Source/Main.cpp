0abece4611dea52221f8b795e88fc6dc005605a3
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2021-08-15 21:30:53 +0500

    SDL Added

--- GarajEngine/Source/Main.cpp
+++ GarajEngine/Source/Main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
 #undef main
 
 int main() {
 	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow(
+		"Garaj Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		640, 480, SDL_WINDOW_RESIZABLE);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 240, 248, 255, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(3000);
+
 	return 0;
 }
