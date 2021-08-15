0eeb251364eeb15c5c1add945936d6b46194d356
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2021-08-16 02:01:51 +0500

    Alignment

--- CPU_Renderer/Source/Main.cpp
+++ CPU_Renderer/Source/Main.cpp
@@ -8,8 +8,8 @@
 int main() {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* window = SDL_CreateWindow(
-		"Garaj Engine [CPU Renderer]", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		640, 480, SDL_WINDOW_RESIZABLE);
+		"Garaj Engine [CPU Renderer]", SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_RESIZABLE);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
 	SDL_SetRenderDrawColor(renderer, 240, 248, 255, 255);
