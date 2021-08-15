8d02185f299a3a636b5aa428308fdd31d15238cd
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2021-08-16 01:16:51 +0500

    Code swaps and startup message for garaj engine

--- GarajEngine/Source/Main.cpp
+++ GarajEngine/Source/Main.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
+#include <string>
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
 #include <SDL_mixer.h>
 #undef main
 
+void startup_message() {
+	std::string platform = "32 bit";
+	std::string configuration = "release";
+#ifdef _WIN64
+	platform = "64 bit";
+#endif
+#ifdef _DEBUG
+	configuration = "debug";
+#endif
+	std::cout << "Garaj Engine " << configuration << " version running on a " << platform << " platform" << std::endl;
+}
+
 int main() {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = SDL_CreateWindow(
-		"Garaj Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		640, 480, SDL_WINDOW_RESIZABLE);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-
-	SDL_SetRenderDrawColor(renderer, 240, 248, 255, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-
-	SDL_Delay(3000);
-
+	startup_message();
 	return 0;
 }
