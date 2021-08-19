7599db1a16098acc43e783f19a3705f120a93f04
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2021-08-19 22:19:50 +0500

    Vector Operations

--- CPU_Renderer/Source/Vector/Vector.cpp
+++ CPU_Renderer/Source/Vector/Vector.cpp
@@ -19,6 +19,26 @@ float vec2_length(vec2_t v) {
 	return (float)sqrt(h_squared);
 }
 
+vec2_t vec2_add(vec2_t v1, vec2_t v2) {
+	vec2_t result = { v1.x + v2.x, v1.y + v2.y };
+	return result;
+}
+
+vec2_t vec2_subtract(vec2_t v1, vec2_t v2) {
+	vec2_t result = { v1.x - v2.x, v1.y - v2.y };
+	return result;
+}
+
+vec2_t vec2_multiply(vec2_t v, float scaler) {
+	vec2_t result = { v.x * scaler, v.y * scaler };
+	return result;
+}
+
+vec2_t vec2_divide(vec2_t v, float scaler) {
+	vec2_t result = { v.x / scaler, v.y / scaler };
+	return result;
+}
+
 ///////////////////////
 // 3D Vector functions
 //////////////////////
@@ -53,4 +73,24 @@ vec3_t vec3_rotate_z(vec3_t v, float angle) {
 float vec3_length(vec3_t v) {
 	float h_squared = (v.x * v.x) + (v.y * v.y) + (v.z * v.z);
 	return (float)sqrt(h_squared);
+}
+
+vec3_t vec3_add(vec3_t v1, vec3_t v2) {
+	vec3_t result = { v1.x + v2.x, v1.y + v2.y, v1.z + v2.z };
+	return result;
+}
+
+vec3_t vec3_subtract(vec3_t v1, vec3_t v2) {
+	vec3_t result = { v1.x - v2.x, v1.y - v2.y, v1.z - v2.z };
+	return result;
+}
+
+vec3_t vec3_multiply(vec3_t v, float scaler) {
+	vec3_t result = { v.x * scaler, v.y * scaler, v.z * scaler };
+	return result;
+}
+
+vec3_t vec3_divide(vec3_t v, float scaler) {
+	vec3_t result = { v.x / scaler, v.y / scaler, v.z / scaler };
+	return result;
 }
--- CPU_Renderer/Source/Vector/Vector.h
+++ CPU_Renderer/Source/Vector/Vector.h
@@ -19,7 +19,11 @@ typedef struct {
 //////////////////////
 
 float vec2_length(vec2_t v);
-vec2_t vec2_rotate(vec2_t v);
+vec2_t vec2_rotate(vec2_t v, float angle);
+vec2_t vec2_add(vec2_t v1, vec2_t v2);
+vec2_t vec2_subtract(vec2_t v1, vec2_t v2);
+vec2_t vec2_multiply(vec2_t v, float scaler);
+vec2_t vec2_divide(vec2_t v, float scaler);
 
 ///////////////////////
 // 3D Vector functions
@@ -29,5 +33,9 @@ vec3_t vec3_rotate_x(vec3_t v, float angle);
 vec3_t vec3_rotate_y(vec3_t v, float angle);
 vec3_t vec3_rotate_z(vec3_t v, float angle);
 float vec3_length(vec3_t v);
+vec3_t vec3_add(vec3_t v1, vec3_t v2);
+vec3_t vec3_subtract(vec3_t v1, vec3_t v2);
+vec3_t vec3_multiply(vec3_t v, float scaler);
+vec3_t vec3_divide(vec3_t v, float scaler);
 
 #endif
