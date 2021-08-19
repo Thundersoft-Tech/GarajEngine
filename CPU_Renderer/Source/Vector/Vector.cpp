4716347c8fe61f63ab82bed63090592a21721837
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2021-08-19 21:45:39 +0500

    Vector File update

--- CPU_Renderer/Source/Vector/Vector.cpp
+++ CPU_Renderer/Source/Vector/Vector.cpp
@@ -1,6 +1,28 @@
 #include <math.h>
 #include "Vector.h"
 
+
+///////////////////////
+// 2D Vector functions
+//////////////////////
+
+vec2_t vec2_rotate(vec2_t v, float angle) {
+	vec2_t rotated_vector = {
+		v.x * cos(angle) - v.y * sin(angle),
+		v.y * cos(angle) + v.x * sin(angle),
+	};
+	return rotated_vector;
+}
+
+float vec2_length(vec2_t v) {
+	float h_squared = (v.x * v.x) + (v.y * v.y);
+	return (float)sqrt(h_squared);
+}
+
+///////////////////////
+// 3D Vector functions
+//////////////////////
+
 vec3_t vec3_rotate_x(vec3_t v, float angle) {
 	vec3_t rotated_vector = {
 		v.x,
@@ -26,4 +48,9 @@ vec3_t vec3_rotate_z(vec3_t v, float angle) {
 		v.z
 	};
 	return rotated_vector;
+}
+
+float vec3_length(vec3_t v) {
+	float h_squared = (v.x * v.x) + (v.y * v.y) + (v.z * v.z);
+	return (float)sqrt(h_squared);
 }
--- CPU_Renderer/Source/Vector/Vector.h
+++ CPU_Renderer/Source/Vector/Vector.h
@@ -13,8 +13,21 @@ typedef struct {
 } vec3_t;
 
 // Function signatures to manipulate vectors
+
+///////////////////////
+// 2D Vector functions
+//////////////////////
+
+float vec2_length(vec2_t v);
+vec2_t vec2_rotate(vec2_t v);
+
+///////////////////////
+// 3D Vector functions
+//////////////////////
+
 vec3_t vec3_rotate_x(vec3_t v, float angle);
 vec3_t vec3_rotate_y(vec3_t v, float angle);
 vec3_t vec3_rotate_z(vec3_t v, float angle);
+float vec3_length(vec3_t v);
 
 #endif
